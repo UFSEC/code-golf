f5836186b25687a437ac3c70c8fbe1dbd1615375
NeverDoubtTheWorm <sthomas93@ufl.edu>
2014-10-08 19:33:07 -0400

    QuickFix

--- NickT - Copy/3.py
+++ NickT - Copy/3.py
@@ -1,2 +1,2 @@
-def f(a): return 0 if a == 0 else 1 if a == 1 else f(a-1)+f(a-2)
+def f(a): return a if a <= 1 else f(a-1)+f(a-2)
 for x in range(12): print  f(x+1),
--- NickT - Copy/4.py
+++ NickT - Copy/4.py
@@ -1,1 +1,1 @@
-for a in range(97,123):	print(chr(a)),
+for a in range(97,123):	print chr(a),
