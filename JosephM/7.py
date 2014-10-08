eea70ce8fcceac8dbd117cdc215049265b26a465
Joseph McConnell <jjmdevelops@gmail.com>
2014-10-08 19:58:14 -0400

    AN O GRAMMY AWARD WINNING 3.0

--- JosephM/7.py
+++ JosephM/7.py
@@ -1,8 +1,8 @@
 import sys
-a = sys.argv[1]
-b = sys.argv[2]
-F = True
+s=sys.argv
+a=s[1]
+b=s[2]
+F=True
 for x in (0,len(a)-1):
-	if(a[x] not in b):
-		F = False
+	if(a[x] not in b):F=False
 print F
